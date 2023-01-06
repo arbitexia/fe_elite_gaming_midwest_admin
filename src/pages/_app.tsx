785570f8dd681da880bdad95dc4973a4155dbd6e
iritep <rpat.upwork@gmail.com>
2023-01-05 18:24:40 -0600

    fix: redirect issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import { useStore } from 'react-redux';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 import { wrapper } from '@/redux/store';
@@ -8,11 +8,20 @@ import { AppToastProvider, AppThemeProvider } from '@/providers';
 
 function EliteApp({ Component, pageProps }: AppProps) {
   const store = useStore();
-  const router = useRouter();
-  const path = (/#!(\/.*)$/.exec(router.asPath) || [])[1];
-  if (path) {
-    router.replace(path, { query: router.query });
+  // const router = useRouter();
+  if (typeof window.history.pushState == 'function') {
+    window.history.pushState(
+      null,
+      'admin.elitegaming.rpatdev.com',
+      window.location.hash.substring(2)
+    );
+  } else {
+    window.location.hash = window.location.hash.substring(2);
   }
+  // const path = (/#!(\/.*)$/.exec(router.asPath) || [])[1];
+  // if (path) {
+  //   router.replace(path, { query: router.query });
+  // }
   return (
     <AppThemeProvider>
       <AppToastProvider>
