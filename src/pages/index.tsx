e72f7b69767071916b6d2dbf7448390d99430c20
iritep <rpat.upwork@gmail.com>
2023-01-06 13:57:00 -0600

    test: routing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,12 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   const router = useRouter();
+  console.log(router);
   if (router.pathname === '/') {
-    router.replace('/users/customers');
+    // router.replace('/users/customers');
     return <DashboardLayout>Dashboard</DashboardLayout>;
   } else {
-    router.replace(router.asPath);
+    // router.replace(router.asPath);
     return <></>;
   }
 };
