f916774abe02b0bb34f532842fcd5ce9aafc5007
iritep <rpat.upwork@gmail.com>
2023-01-06 16:40:48 -0600

    test: deploy redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,15 +4,8 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   const router = useRouter();
-  console.log(router);
-  const path = router.asPath;
-  if (path === '/' || path.includes('/?')) {
-    router.replace('/users/customers');
-    return <DashboardLayout>Dashboard</DashboardLayout>;
-  } else {
-    router.replace(router.asPath);
-    return <></>;
-  }
+  router.replace('/users/customers');
+  return <DashboardLayout>Dashboard</DashboardLayout>;
 };
 
 export default Home;
