0606126eed467eec744b310c454077252d31fcc7
Federico <federicodido525@gmail.com>
2023-03-12 07:51:31 -0700

    remove unused variable

--- src/pages/rewards/index.tsx
+++ src/pages/rewards/index.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Divider } from '@mui/material';
 import {
-  UIFlexSpaceBox,
   UIFlexWrapBox,
   UIOptionMenuItem,
   UISelectMenuItem,
