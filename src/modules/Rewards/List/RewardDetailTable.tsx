0d7e13508f11cad31905dec616a039c26e4f441e
Federico <federicodido525@gmail.com>
2023-03-13 22:38:51 -0700

    reward detail table date format

--- src/modules/Rewards/List/RewardDetailTable.tsx
+++ src/modules/Rewards/List/RewardDetailTable.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Table, TableHead, TableBody } from '@mui/material';
+import { format } from 'date-fns';
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableContainer,
+  Paper,
+} from '@mui/material';
 import { Product } from '@/types';
 import { UIChip, UIListTableCell, UIListTableRow } from '@/components/UI';
 import { getColor } from '@/libs/data-helper';
@@ -10,36 +17,47 @@ interface IRewardDetailTable {
 
 const RewardDetailTable = ({ rewards }: IRewardDetailTable) => {
   return (
-    <Table size="small">
-      <TableHead>
-        <UIListTableRow>
-          <UIListTableCell>ID</UIListTableCell>
-          <UIListTableCell>Name</UIListTableCell>
-          <UIListTableCell>Detail</UIListTableCell>
-          <UIListTableCell>Point</UIListTableCell>
-          <UIListTableCell>Status</UIListTableCell>
-          <UIListTableCell>Due Date</UIListTableCell>
-        </UIListTableRow>
-      </TableHead>
-      <TableBody>
-        {rewards.map((reward, index) => {
-          return (
-            <UIListTableRow key={`reward-${reward.id}`}>
-              <UIListTableCell>{index + 1}</UIListTableCell>
-              <UIListTableCell>{reward.name}</UIListTableCell>
-              <UIListTableCell sx={{ color: 'rgba(0, 0, 0, 0.3) !important' }}>
-                {reward.short}
-              </UIListTableCell>
-              <UIListTableCell>{reward.point}</UIListTableCell>
-              <UIListTableCell align="center">
-                <UIChip label={reward.status} color={getColor(reward.status)} />
-              </UIListTableCell>
-              <UIListTableCell>{reward.createdAt}</UIListTableCell>
-            </UIListTableRow>
-          );
-        })}
-      </TableBody>
-    </Table>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <UIListTableRow>
+            <UIListTableCell>ID</UIListTableCell>
+            <UIListTableCell>Name</UIListTableCell>
+            <UIListTableCell>Detail</UIListTableCell>
+            <UIListTableCell>Point</UIListTableCell>
+            <UIListTableCell>Status</UIListTableCell>
+            <UIListTableCell>Due Date</UIListTableCell>
+          </UIListTableRow>
+        </TableHead>
+        <TableBody>
+          {rewards.map((reward, index) => {
+            return (
+              <UIListTableRow key={`reward-${reward.id}`}>
+                <UIListTableCell>{index + 1}</UIListTableCell>
+                <UIListTableCell>{reward.name}</UIListTableCell>
+                <UIListTableCell
+                  sx={{ color: 'rgba(0, 0, 0, 0.3) !important' }}
+                >
+                  {reward.short}
+                </UIListTableCell>
+                <UIListTableCell>{reward.point}</UIListTableCell>
+                <UIListTableCell>
+                  <UIChip
+                    label={reward.status}
+                    color={getColor(reward.status)}
+                  />
+                </UIListTableCell>
+                <UIListTableCell>
+                  {reward.createdAt
+                    ? format(new Date(reward.createdAt), 'yyyy-MM-dd')
+                    : ''}
+                </UIListTableCell>
+              </UIListTableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
